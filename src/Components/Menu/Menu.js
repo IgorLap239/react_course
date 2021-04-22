10f16787e8aa9ab25c0ce974a1691a279123dae5
Igor <ilaptev@ya.ru>
2021-04-22 22:26:54 +0300

    Урок 14. Получение данных с помощью fetch

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import bannerImg from '../../img/banner.png';
+import { useFetch } from '../Hooks/useFetch';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -23,21 +23,35 @@ const SectionMenu = styled.section`
 `;
 
 
-export const Menu = ({ setOpenItem }) => (
-  <MenuStyled>
-    <BannerMenu>
-    </BannerMenu>
-    <SectionMenu>
-      <h2>Бургеры</h2>
-      <ListItem itemList={dbMenu.burger}
-        setOpenItem={setOpenItem}
-      />
-    </SectionMenu>
-    <SectionMenu>
-      <h2>Закуски / Напитки</h2>
-      <ListItem itemList={dbMenu.other}
-        setOpenItem={ setOpenItem }
-      />
-    </SectionMenu>
-  </MenuStyled>
-);
+export const Menu = ({ setOpenItem }) => {
+  
+  const res = useFetch();
+
+  const dbMenu = res.response;
+
+  
+  return (
+    <MenuStyled>
+      <BannerMenu>
+      </BannerMenu>
+      { res.response ? 
+        <>
+          <SectionMenu>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger}
+              setOpenItem={setOpenItem}
+            />
+          </SectionMenu>
+          <SectionMenu>
+            <h2>Закуски / Напитки</h2>
+            <ListItem itemList={dbMenu.other}
+              setOpenItem={ setOpenItem }
+            />
+          </SectionMenu>
+        </> : res.error ?
+        <div>Sorry..</div> :
+        <div>Loading...</div>
+      }
+    </MenuStyled>
+  )
+};
